f631861d4cfa0c5e3fa698bd9b53079841b6777b
danielmitiku <wedanielmitiku@gmail.com>
2019-12-18 16:38:45 +0300

    added more tests for caesar_cipher

--- tests/caesar_cipher.test.js
+++ tests/caesar_cipher.test.js
@@ -1,9 +1,17 @@
 import caesarCipher from "../src/caesar_cipher";
 
-test('shift 1', () => {
+test('shifting 1', () => {
   expect(caesarCipher('aaa', 1)).toBe('bbb');
 });
 
 test('wrapping', () => {
   expect(caesarCipher('zzz', 1)).toBe('aaa');
+});
+
+test('case sensitivity', () => {
+  expect(caesarCipher('Daniel', 1)).toBe('Ebojfm');
+});
+
+test('reserves punctuation', () => {
+  expect(caesarCipher('Moses!', 20)).toBe('Gimym!');
 });
