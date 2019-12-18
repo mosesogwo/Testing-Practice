663e13d931a8ce83df6a5391f7f8d13efce2a8b8
Moses Ogwo <mosesogwo@gmail.com>
2019-12-18 11:55:35 +0100

    add reverse_string, calculator and array_analysis methods and test

---
+++ src/array_analysis.js
@@ -0,0 +1,10 @@
+const arrayAnalysis = (array) => {
+  return {
+    "average": (array.reduce((acc, x) => acc + x, 0))/array.length,
+    "min": Math.min(...array),
+    "max": Math.max(...array),
+    "length": array.length
+  }
+}
+
+export default arrayAnalysis;
